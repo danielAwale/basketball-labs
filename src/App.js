d97331177731435ee633011e6ffaf68977bf212f
danielAwale <danielmadhukarawale@gmail.com>
2022-11-16 15:27:44 -0800

    contain the api code snippet to get the player info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
 
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
+// 		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://api-nba-v1.p.rapidapi.com/players?search=james', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
 function App() {
   return (
     <div className="App">

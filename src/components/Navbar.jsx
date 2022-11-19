c5b980bbc8c2c9da0e6c1aecc3bb9027bd27f7d1
danielAwale <danielmadhukarawale@gmail.com>
2022-11-18 22:05:24 -0800

    contain the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center h-24 mx-auto px-4 max-w-[1240px] text-fuchsia-50'>
-
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>BasketBall Labs</h1>
-      <ul className='flex'>
-        <li className='p-4'>Graphs</li>
-        <li className='p-4'>Stats</li>
-        <li className='p-4'>Watchlist</li>
-        <li className='p-4'>Register</li>
-      </ul>
+    <div>
+      {/* Navbar */}
+      <nav className="relative container mx-auto p-6">
+      {/* Flex container */}
+      <div className="flex items-center justify-between">
+        {/* Logo */}
+        <div className="pt2">
+          <h1 className='text-l font-press-start'>BasketBall Labs</h1>
+        </div>
+        {/* Menu items */}
+        <div className="hidden md:flex space-x-11">
+          <a href="#" className='text-lg hover:text-gray-200'>Graphs</a>
+          <a href="#" className='text-lg hover:text-gray-200'>Stats</a>
+          <a href="#" className='text-lg hover:text-gray-200'>Watchlist</a>
+        </div>
+        {/* Butto */}
+        <a href="" className="p-3 px-6 pt-2 text-white bg-orange-600 rounded-full baseline text-lg">Register</a>
+      </div>
+      </nav>
     </div>
   )
 }

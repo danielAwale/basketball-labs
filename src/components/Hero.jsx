b4c829bdb51851c6e79f0f61e142de1d9de81e71
danielAwale <danielmadhukarawale@gmail.com>
2022-11-22 11:03:14 -0800

    save data

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Hero = () => {
-  return (
-    <div></div>
-  )
-}
+  const [news, setNews] = useState([]);
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
+      'X-RapidAPI-Host': 'nba-latest-news.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://nba-latest-news.p.rapidapi.com/news/player/lebron-james', options)
+    .then(response => response.json())
+    .then(data => setNews(data))
+    .catch(err => console.error(err));
+
+    useEffect(() => {
+      fetch()
+    }, [])
+
+
+    return (
+      <div>
+            {news.map(new1 => (
+              <>
+                <h2>{new1.title}</h2>
+                <a href={new1.url}>Learn More</a>
+                <p>{new1.source}</p>
+              </>
+            ))}
+      </div>
+    )
+  }
 
 export default Hero

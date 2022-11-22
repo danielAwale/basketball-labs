91d256b0f847626bc552193076f89a871981c41b
Jas Mann <jasforshort@hotmail.ca>
2022-11-22 19:53:16 +0000

    Stash

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // const options = {
 // 	method: 'GET',
 // 	headers: {
-// 		'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
+// 		'X-RapidAPI-Key': RAPID_API_KEY,
 // 		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
 // 	}
 // };

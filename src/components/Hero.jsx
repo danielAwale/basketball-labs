5be909ad70b7dbca2ba0fbf65cd9797870e73707
danielAwale <danielmadhukarawale@gmail.com>
2022-11-23 10:26:50 -0800

    hero add cards

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,44 @@
 import React, { useState, useEffect } from 'react'
 
+
 const Hero = () => {
-  const [news, setNews] = useState([]);
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
-      'X-RapidAPI-Host': 'nba-latest-news.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://nba-latest-news.p.rapidapi.com/news/player/lebron-james', options)
-    .then(response => response.json())
-    .then(data => setNews(data))
-    .catch(err => console.error(err));
-
-    useEffect(() => {
-      fetch()
-    }, [])
-
-
     return (
-      <div>
-
-        
-            {news.map(new1 => (
-              <>
-                <h2>{new1.title}</h2>
-                <a href={new1.url}>Learn More</a>
-                <p>{new1.source}</p>
-              </>
-            ))}
+      <>
+      <div></div>
+      <div class="p-10">  
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="../lebron.png" alt="Mountain" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">LeBron James</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#rebounds</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#points</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#assists</span>
+        </div>
       </div>
+    </div>
+
+    <div class="p-10">  
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="../lebron.png" alt="Mountain" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">LeBron James</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#rebounds</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#points</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#assists</span>
+        </div>
+      </div>
+    </div>
+    </>
     )
   }
 

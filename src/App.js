780b011d95bb03e9aae3450d39c072bf61e44e0d
danielAwale <danielmadhukarawale@gmail.com>
2022-11-23 09:36:06 -0800

    working on styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import FeaturedPlayers from './components/FeaturedPlayers';
-import Nav from './components/Nav.js';
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'c31c0f80f4mshbb6b108a22becd7p1ccc72jsn990e3c358a6d',
-// 		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://api-nba-v1.p.rapidapi.com/players?search=james', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <Nav/>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 

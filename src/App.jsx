21ed26c423ebf04c6656c313334bc366f413e6c1
danielAwale <danielmadhukarawale@gmail.com>
2022-11-28 09:04:15 -0800

    got the bar chart to work yay

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import BarChartStats from './components/BarChartStats';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import News from './components/News';
 import Stats from './components/Stats';
+import {PlayerData} from './Data'
 
 function App() {
+
+const [userData, setUserData] = useState({
+  labels: PlayerData.map(item => item.first_name),
+  datasets: [
+    {
+      label: 'Player Points',
+      data: PlayerData.map(item => item.points)
+    },
+    {
+      label: 'Player Assists',
+      data: PlayerData.map(item => item.assists)
+    },
+    {
+      label: 'Player Rebounds',
+      data: PlayerData.map(item => item.rebounds)
+    }
+  ]
+})
+
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <Stats />
       <News />
+      <BarChartStats chartData={userData} />
       <Footer />
     </div>
   );
---
+++ src/components/BarChartStats.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Bar} from 'react-chartjs-2'
+import {Chart as chartJS} from 'chart.js/auto'
+
+const BarChartStats = ({chartData}) => {
+  return (
+    <div style={{width:1200}}>
+      <Bar data={chartData} />
+    </div>
+  )
+}
+
+export default BarChartStats

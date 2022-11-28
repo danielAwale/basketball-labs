b8e330719e9fef370ac20901f915ecfb79aac785
Noah Palin <noah.m.palin@gmail.com>
2022-11-28 18:46:22 +0000

    Changes to navbar and featured-playeer tiles.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import './App.css';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import News from './components/News';
+// import News from './components/News';
+import Nav from './components/Nav';
+import Featured_Players from './components/Featured_Players';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
+      <Nav />
+      <Featured_Players />
       <section>
-      <News />
       </section>
 
       <section>

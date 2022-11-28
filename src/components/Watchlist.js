5dc716e6dc3ae4dd0d8dbaaf7d4c89b43e1ae1a9
danielAwale <danielmadhukarawale@gmail.com>
2022-11-28 11:15:28 -0800

    changes from dashboard to watchlist

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState, useEffect } from "react";
 
-const Dashboard = ({ setAuth }) => {
+const Watchlist = ({ setAuth }) => {
 
   const [name, setName] = useState("");
 
   async function getName() {
     try {
-      const response = await fetch("http://localhost:5000/dashboard/", {
+      const response = await fetch("http://localhost:5000/watchlist/", {
         method: "GET",
         headers: { token: localStorage.jwt_token }
       });
@@ -34,10 +34,10 @@ const Dashboard = ({ setAuth }) => {
 
   return (
     <Fragment>
-      <h1>Dashboard {name}</h1>
+      <h1>Watchlist {name}</h1>
       <button className="btn btn-primary" onClick={e => logout(e)}>Logout</button>
     </Fragment>
   );
 };
 
-export default Dashboard;
+export default Watchlist;

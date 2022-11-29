154bbc2b60b7effb18ad5efcf93b64ada0535195
danielAwale <danielmadhukarawale@gmail.com>
2022-11-29 13:20:14 -0800

    doesn't work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Stats from "./components/Stats";
@@ -14,15 +14,13 @@ function App() {
   };
   return (
     <>
-     <Router>
-            <Switch>
+        <Routes>
             <Route path="/" element={<Home />} />
-            <Route exact path="/login" render={props => !isAuthenicated ? <Login {...props} setAuth={setAuth} /> : <Redirect to="/watchlist" />} />
-            <Route exact path="/register" render={props => !isAuthenicated ? <Register {...props} setAuth={setAuth} /> : <Redirect to="/login" />} />
-            <Route exact path="/watchlist" render={props => isAuthenicated ? <Watchlist {...props} setAuth={setAuth} /> : <Redirect to="/login" />} />
+            {/* <Route path="/login" render={props => !isAuthenicated ? <Login {...props} setAuth={setAuth} /> : <Redirect to="/watchlist" />} />
+            <Route path="/register" render={props => !isAuthenicated ? <Register {...props} setAuth={setAuth} /> : <Redirect to="/login" />} />
+            <Route path="/watchlist" render={props => isAuthenicated ? <Watchlist {...props} setAuth={setAuth} /> : <Redirect to="/login" />} /> */}
             <Route path="/stats" element={<Stats />} />
-          </Switch>
-    </Router>
+        </Routes>
     </>
   );
 }

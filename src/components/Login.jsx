7c8c26a410f8e84bde2f50499402578187dfb116
Jas Mann <jasforshort@hotmail.ca>
2022-11-29 20:19:22 +0000

    StyleC login/register page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import loginImg from '../assets/login.jpeg';
 
 const Login = ({ setAuth }) => {
 
@@ -46,15 +47,44 @@ const Login = ({ setAuth }) => {
 
   return (
     <Fragment>
-      <h1 className="" text-center my-5>Login</h1>
-      <form onSubmit={onSubmitForm}>
-        <input type="email" name="email" placeholder="email" className="form-control my-3" value={email} onChange={e => onChange(e)} />
-        <input type="password" name="password" placeholder="password" className="form-control my-3" value={password} onChange={e => onChange(e)} />
-        <button className btn btn-success btn-block>Submit</button>
-      </form>
-      <Link to="/register">Register</Link>
+      <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+        <div className='hidden sm:block'>
+          <img className='w-full h-full object-cover' src={loginImg} alt="" />
+        </div>
+
+        <div className='bg-gray-100 flex flex-col justify-center'>
+          <form onSubmit={onSubmitForm} className='max-w-[400px] w-full mx-auto bg-white p-4'>
+            <h2 className='text-4xl font-bold text-center py-6'>Basketball Labs</h2>
+            <div className='flex flex-col py-2'>
+              <label>Email</label>
+              <input className='border p-2' name ="email" type="email" placeholder="Email" value={email} onChange={e => onChange(e)}/>
+            </div>
+            <div className='flex flex-col py-2'>
+              <label>Password</label>
+              <input className='border p-2' name ="password" type="password" placeholder="Password" value={password} onChange={e => onChange(e)} />
+            </div>
+            <button className='border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white'>Sign In</button>
+            <div className='flex justify-between'>
+              <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
+              <a href="/register" class="text-blue-500 underline">Create an account!</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </Fragment>
-  );
+  )
 };
 
 export default Login;
+
+// return (
+//   <Fragment>
+//     <h1 className="" text-center my-5>Login</h1>
+//     <form onSubmit={onSubmitForm}>
+//       <input type="email" name="email" placeholder="email" className="form-control my-3" value={email} onChange={e => onChange(e)} />
+//       <input type="password" name="password" placeholder="password" className="form-control my-3" value={password} onChange={e => onChange(e)} />
+//       <button className btn btn-success btn-block>Submit</button>
+//     </form>
+//     <Link to="/register">Register</Link>
+//   </Fragment>
+// );
